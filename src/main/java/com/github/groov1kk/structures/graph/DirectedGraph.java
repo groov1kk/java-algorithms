f95f2c12519aa4a91df9b0ea65c300acb473def5
Ilya Koshaleu <ilyako1988@gmail.com>
2022-10-26 17:21:35 +0200

    add graphs copy constructors

--- src/main/java/com/github/groov1kk/structures/graph/DirectedGraph.java
+++ src/main/java/com/github/groov1kk/structures/graph/DirectedGraph.java
@@ -28,7 +28,7 @@ import com.github.groov1kk.structures.graph.algorithms.TopologicalSort;
 @NotThreadSafe
 public class DirectedGraph<V> implements Graph<V> {
 
-  private final Map<V, List<V>> adjacent = new HashMap<>();
+  private final Map<V, List<V>> adjacent;
   private final BiPredicate<V, V> strategy;
 
   private int edges;
@@ -39,6 +39,14 @@ public class DirectedGraph<V> implements Graph<V> {
 
   public DirectedGraph(BiPredicate<V, V> strategy) {
     this.strategy = Objects.requireNonNull(strategy);
+    this.adjacent = new HashMap<>();
+  }
+
+  public DirectedGraph(DirectedGraph<V> graph) {
+    Objects.requireNonNull(graph, "Graph must not be null");
+    this.adjacent = new HashMap<>(graph.adjacent);
+    this.strategy = graph.strategy;
+    this.edges = graph.edges;
   }
 
   @Override
@@ -238,7 +246,7 @@ public class DirectedGraph<V> implements Graph<V> {
     }
 
     public DirectedGraph<V> build() {
-      return graph;
+      return new DirectedGraph<>(graph);
     }
   }
 }
